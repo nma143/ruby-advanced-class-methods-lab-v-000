be36a4bbaf314c3b3b2cc5213ed4d78ef1b9b801
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-27 01:51:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,13 +38,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-
-    if ! self.find_by_name(name)
-      self.create_by_name(name)
+    newsong =  self.find_by_name(name)
+    if !newsong
+      newsong = self.create_by_name(name)
     end
-
+    newsong
   end
-
-
 
 end
