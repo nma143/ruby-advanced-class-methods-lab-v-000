c7ece62459d44dd41acf8f372872eca9c1bb2fa6
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-27 01:48:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,14 @@ class Song
 
   def self.find_by_name(name)
     self.all.detect{|song| song.name==name}
+  end
 
+  def self.find_or_create_by_name(name)
+
+    if ! (self.all.detect{|song| song.name==name})
+      self.create_by_name(name)
+    end
+    
   end
 
 
